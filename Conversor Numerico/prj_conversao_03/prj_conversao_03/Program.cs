83ab5419fe04651ab98db6d635561e19aeb5be97
André Moutinho <68257070+AMoutinho@users.noreply.github.com>
2024-08-26 17:42:39 -0300

    Conversor Numerico

---
+++ Conversor Numerico/prj_conversao_03/prj_conversao_03/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace prj_conversao_03
+{
+    static class Program
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmconversao());
+        }
+    }
+}
