6eec70201bba440496322bfa7f4555e93efc231f
André Moutinho <68257070+AMoutinho@users.noreply.github.com>
2024-08-26 17:45:18 -0300

    Conversor Numerico

---
+++ Conversor Numerico/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace prj_conversao_03
+{
+    partial class frmconversao
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">verdade se for necessário descartar os recursos gerenciados; caso contrário, falso.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte do Designer - não modifique
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nudBaseO = new System.Windows.Forms.NumericUpDown();
+            this.txt_origem_num = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.nudBaseR = new System.Windows.Forms.NumericUpDown();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_resultado = new System.Windows.Forms.TextBox();
+            this.btn_converte = new System.Windows.Forms.Button();
+            this.btn_limpar = new System.Windows.Forms.Button();
+            this.btn_sair = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudBaseO)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudBaseR)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.nudBaseO);
+            this.groupBox1.Controls.Add(this.txt_origem_num);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(5, 7);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(237, 85);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Número de Entrada";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Base do Valor Inserido";
+            // 
+            // nudBaseO
+            // 
+            this.nudBaseO.Location = new System.Drawing.Point(163, 55);
+            this.nudBaseO.Name = "nudBaseO";
+            this.nudBaseO.Size = new System.Drawing.Size(59, 22);
+            this.nudBaseO.TabIndex = 3;
+            this.toolTip1.SetToolTip(this.nudBaseO, "Informe a base do número digitado (binário,hexadecimal,decimal,octal e etc)");
+            // 
+            // txt_origem_num
+            // 
+            this.txt_origem_num.Location = new System.Drawing.Point(114, 24);
+            this.txt_origem_num.Name = "txt_origem_num";
+            this.txt_origem_num.Size = new System.Drawing.Size(108, 22);
+            this.txt_origem_num.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Digite um Valor";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // nudBaseR
+            // 
+            this.nudBaseR.Location = new System.Drawing.Point(163, 79);
+            this.nudBaseR.Name = "nudBaseR";
+            this.nudBaseR.Size = new System.Drawing.Size(59, 22);
+            this.nudBaseR.TabIndex = 5;
+            this.toolTip1.SetToolTip(this.nudBaseR, "Informe a base em que deseja ter o resultado (decimal,binario,octal,hexa,etc)");
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.nudBaseR);
+            this.groupBox2.Controls.Add(this.txt_resultado);
+            this.groupBox2.Location = new System.Drawing.Point(5, 98);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(237, 110);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Resultado";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(128, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Base do Resultado";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(185, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Conversão do valor inserido";
+            // 
+            // txt_resultado
+            // 
+            this.txt_resultado.Enabled = false;
+            this.txt_resultado.Location = new System.Drawing.Point(58, 50);
+            this.txt_resultado.Name = "txt_resultado";
+            this.txt_resultado.Size = new System.Drawing.Size(108, 22);
+            this.txt_resultado.TabIndex = 5;
+            // 
+            // btn_converte
+            // 
+            this.btn_converte.Location = new System.Drawing.Point(3, 217);
+            this.btn_converte.Name = "btn_converte";
+            this.btn_converte.Size = new System.Drawing.Size(81, 26);
+            this.btn_converte.TabIndex = 7;
+            this.btn_converte.Text = "Converter";
+            this.btn_converte.UseVisualStyleBackColor = true;
+            this.btn_converte.Click += new System.EventHandler(this.btn_converte_Click);
+            // 
+            // btn_limpar
+            // 
+            this.btn_limpar.Location = new System.Drawing.Point(86, 217);
+            this.btn_limpar.Name = "btn_limpar";
+            this.btn_limpar.Size = new System.Drawing.Size(78, 26);
+            this.btn_limpar.TabIndex = 8;
+            this.btn_limpar.Text = "Limpar";
+            this.btn_limpar.UseVisualStyleBackColor = true;
+            this.btn_limpar.Click += new System.EventHandler(this.btn_limpar_Click);
+            // 
+            // btn_sair
+            // 
+            this.btn_sair.Location = new System.Drawing.Point(166, 217);
+            this.btn_sair.Name = "btn_sair";
+            this.btn_sair.Size = new System.Drawing.Size(76, 26);
+            this.btn_sair.TabIndex = 9;
+            this.btn_sair.Text = "Sair";
+            this.btn_sair.UseVisualStyleBackColor = true;
+            this.btn_sair.Click += new System.EventHandler(this.btn_sair_Click);
+            // 
+            // frmconversao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(246, 252);
+            this.ControlBox = false;
+            this.Controls.Add(this.btn_sair);
+            this.Controls.Add(this.btn_limpar);
+            this.Controls.Add(this.btn_converte);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmconversao";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Conversão";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudBaseO)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudBaseR)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown nudBaseO;
+        private System.Windows.Forms.TextBox txt_origem_num;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txt_resultado;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown nudBaseR;
+        private System.Windows.Forms.Button btn_converte;
+        private System.Windows.Forms.Button btn_limpar;
+        private System.Windows.Forms.Button btn_sair;
+    }
+}
+
---
+++ Conversor Numerico/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace prj_conversao_03
+{
+    public partial class frmconversao : Form
+    {
+        string caracteres = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ", numDigitado, resFinal, numAux, baseValida;
+        int qtdCaracteres, numAux2, restoMod, resParcial;
+        public frmconversao()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_converte_Click(object sender, EventArgs e)
+        {
+           
+            if (nudBaseO.Value >= 2 && nudBaseR.Value >= 2)
+            {
+                baseValida = "1";
+                resParcial = 0;
+                resFinal = null;
+                txt_resultado.Text = null;
+
+                qtdCaracteres = txt_origem_num.Text.Trim().Length;
+                numDigitado = txt_origem_num.Text.ToUpper();
+
+                
+                for (int a = 0; a < qtdCaracteres; a++)
+                {
+                    
+                    if (baseValida == "1")
+                    {
+                       
+                        for (int x = int.Parse(nudBaseO.Value.ToString()); x < caracteres.Length; x++)
+                        {
+                            
+                            if (!numDigitado.Contains(caracteres[x]))
+                            {
+                                baseValida = "1";
+                            }
+                           
+                            else
+                            {
+                                baseValida = caracteres[x].ToString();
+                                break;
+                            }
+                        }
+                    }
+                }
+                if (baseValida == "1")
+                {
+                    
+                    for (int i = 1; i <= qtdCaracteres; i++)
+                    {
+                       
+                        numAux = numDigitado[qtdCaracteres - i].ToString();
+                        
+                        numAux2 = caracteres.IndexOf(numAux);
+                        resParcial = resParcial + numAux2 * int.Parse(Math.Pow(double.Parse(nudBaseO.Value.ToString()), double.Parse((i - 1).ToString())).ToString());
+                    }
+                    while (resParcial >= double.Parse(nudBaseR.Value.ToString()))
+                    {
+                        
+                        restoMod = resParcial % int.Parse(nudBaseR.Value.ToString());
+                        
+                        resParcial = resParcial / int.Parse(nudBaseR.Value.ToString());
+                        
+                        resFinal = caracteres.Substring(restoMod, 1) + resFinal;
+                    }
+                    
+                    resFinal = caracteres[int.Parse(resParcial.ToString())].ToString() + resFinal;
+                    
+                    txt_resultado.Text = resFinal;
+                }
+                
+                else
+                {
+                    MessageBox.Show("O número digitado possui caracteres além da base de origem informada: " + baseValida, "Base inválida", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+
+        }
+
+        private void btn_limpar_Click(object sender, EventArgs e)
+        {
+            txt_origem_num.Text = ("");
+            txt_resultado.Text = ("");
+        }
+
+        private void btn_sair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+    }
+
+}
+
+    
+
+    
+
